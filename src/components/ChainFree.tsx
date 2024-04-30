52009594f927559c690a637fe3c8f9ed538e00f9
Hamid Roohi <52012062+hamidroohi71@users.noreply.github.com>
2024-04-30 12:45:02 +0330

    fix chain imf

--- src/components/ChainFree.tsx
+++ src/components/ChainFree.tsx
@@ -6,19 +6,13 @@ import HowItWorks from "./HowItWorks";
 
 const ChainFree: React.FC = () => {
   return (
-    <Container classes="mt-[12.5rem] lg:mt-[6.25rem] bg-[url('/images/chainFreebg.png')] bg-cover bg-no-repeat lg:min-h-[106.313rem] relative">
+    <Container classes="mt-[12.5rem] lg:mt-[6.25rem] lg:bg-[url('/images/chainFreebg.png')] bg-contain bg-no-repeat lg:min-h-[106.313rem] relative">
       <div className="flex-col lg:flex-row flex items-center gap-[1.875rem] lg:gap-[6.25rem] lg:flex-wrap">
         {data.map((item) => (
           <ChainFreeItems key={item.id} data={item} />
         ))}
       </div>
-      <div className="w-full flex justify-end mt-[9.375rem] lg:mt-0">
-        <img
-          src="/images/chain.png"
-          alt=""
-          className="lg:w-[52.688rem] lg:h-[46.438rem]"
-        />
-      </div>
+      <div className="lg:hidden bg-[url('/images/chainFreebgMobile.png')] bg-contain w-full min-h-[20.563rem] mt-[9.375rem]"></div>
       <HowItWorks />
     </Container>
   );
